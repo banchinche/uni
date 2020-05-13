23cc6d21365c98f9c23eabee6948ccd7e3a0a764
banchinche <45794357+banchinche@users.noreply.github.com>
2020-05-13 20:59:52 +0300

    const remarks

--- stuff.cpp
+++ stuff.cpp
@@ -3,7 +3,7 @@
 #pragma region Group
 Group::Group()
 {
-	this->groupName = "defaultGroup";
+    this->groupName = "defaultGroup";
 }
 
 Group::~Group()
@@ -12,41 +12,41 @@ Group::~Group()
 
 void Group::show()
 {
-	std::cout << "Group name is" << groupName << std::endl;
+    std::cout << "Group name is" << groupName << std::endl;
 }
 
 std::string Group::get()
 {
-	return groupName;
+    return groupName;
 }
 
 std::ostream & operator<<(std::ostream & out, const Group & group)
 {
-	return out << "Group's name: " << group.groupName << std::endl;
+    return out << "Group's name: " << group.groupName << std::endl;
 }
 
 std::istream & operator >> (std::istream & in, Group & group)
 {
-	std::cout << "Enter group's name" << std::endl;
-	in >> group.groupName;
-	return in;
+    std::cout << "Enter group's name" << std::endl;
+    in >> group.groupName;
+    return in;
 }
 
 std::ofstream & operator<<(std::ofstream & fout, const Group & group)
 {
-	fout << group.groupName << std::endl;
-	return fout;
+    fout << group.groupName << std::endl;
+    return fout;
 }
 std::ifstream & operator >> (std::ifstream & fin, Group & group)
 {
-	fin >> group.groupName;
-	return fin;
+    fin >> group.groupName;
+    return fin;
 }
 #pragma endregion
 #pragma region Audience
 Audience::Audience()
 {
-	audienceNumber = 001;
+    audienceNumber = 001;
 }
 
 Audience::~Audience()
@@ -55,42 +55,42 @@ Audience::~Audience()
 
 void Audience::show()
 {
-	std::cout << "Audience number is " << audienceNumber << std::endl;
+    std::cout << "Audience number is " << audienceNumber << std::endl;
 }
 
 uint64 Audience::get()
 {
-	return audienceNumber;
+    return audienceNumber;
 }
 std::ostream & operator<<(std::ostream & out, const Audience & audience)
 {
-	return out << "Audience number is: " << audience.audienceNumber << std::endl;
+    return out << "Audience number is: " << audience.audienceNumber << std::endl;
 }
 
 std::istream & operator >> (std::istream & in, Audience & audience)
 {
-	std::cout << "Enter audience number" << std::endl;
-	in >> audience.audienceNumber;
-	return in;
+    std::cout << "Enter audience number" << std::endl;
+    in >> audience.audienceNumber;
+    return in;
 }
 
 std::ofstream & operator<<(std::ofstream & fout, const Audience & audience)
 {
-	fout << audience.audienceNumber << std::endl;
-	return fout;
+    fout << audience.audienceNumber << std::endl;
+    return fout;
 }
 
 std::ifstream & operator >> (std::ifstream & fin, Audience & audience)
 {
-		fin >> audience.audienceNumber;
-		return fin;
+        fin >> audience.audienceNumber;
+        return fin;
 }
 
 #pragma endregion
 #pragma region Subject
 Subject::Subject()
 {
-	this->subjectName = "defaultSubject";
+    this->subjectName = "defaultSubject";
 }
 
 Subject::~Subject()
@@ -99,39 +99,39 @@ Subject::~Subject()
 
 void Subject::show()
 {
-	std::cout << "Subject's name is" << subjectName << std::endl;
+    std::cout << "Subject's name is" << subjectName << std::endl;
 }
 std::string Subject::get()
 {
-	return subjectName;
+    return subjectName;
 }
 std::ostream & operator<<(std::ostream & out, const Subject & subject)
 {
-	return out << "Subject's name: " << subject.subjectName << std::endl;
+    return out << "Subject's name: " << subject.subjectName << std::endl;
 }
 std::istream & operator >> (std::istream & in, Subject & subject)
 {
-	std::cout << "Enter subject's name" << std::endl;
-	in >> subject.subjectName;
-	return in;
+    std::cout << "Enter subject's name" << std::endl;
+    in >> subject.subjectName;
+    return in;
 }
 
 std::ofstream & operator<<(std::ofstream & fout, const Subject & subject)
 {
-	fout << subject.subjectName << std::endl;
-	return fout;
+    fout << subject.subjectName << std::endl;
+    return fout;
 }
 
 std::ifstream & operator >> (std::ifstream & fin, Subject & subject)
 {
-	fin >> subject.subjectName;
-	return fin;
+    fin >> subject.subjectName;
+    return fin;
 }
 #pragma endregion
 #pragma region Speciality
 Speciality::Speciality()
 {
-	this->specialityName = "defaultSpeciality";
+    this->specialityName = "defaultSpeciality";
 }
 
 Speciality::~Speciality()
@@ -140,35 +140,35 @@ Speciality::~Speciality()
 
 void Speciality::show()
 {
-	std::cout << "Specialities name is" << specialityName << std::endl;
+    std::cout << "Specialities name is" << specialityName << std::endl;
 }
 
 std::string Speciality::get()
 {
-	return specialityName;
+    return specialityName;
 }
 std::ostream & operator<<(std::ostream & out, const Speciality & speciality)
 {
-	return out << "Specialities name: " << speciality.specialityName << std::endl;
+    return out << "Specialities name: " << speciality.specialityName << std::endl;
 }
 std::istream & operator >> (std::istream & in, Speciality & speciality)
 {
-	std::cout << "Enter specialities name" << std::endl;
-	in >> speciality.specialityName;
-	return in;
+    std::cout << "Enter specialities name" << std::endl;
+    in >> speciality.specialityName;
+    return in;
 }
 std::ofstream & operator<<(std::ofstream & fout, const Speciality & speciality)
 {
-	fout << speciality.specialityName << std::endl;
-	return fout;
+    fout << speciality.specialityName << std::endl;
+    return fout;
 }
 std::ifstream & operator >> (std::ifstream & fin, Speciality & speciality)
 {
-	fin >> speciality.specialityName;
-	return fin;
+    fin >> speciality.specialityName;
+    return fin;
 }
 #pragma endregion
-// jopa 
+// jopa
 #pragma region Groups
 Groups::Groups()
 {
@@ -176,61 +176,61 @@ Groups::Groups()
 
 Groups::Groups(const int size)
 {
-	this->groupsSize = size;
-	groups = new Group[groupsSize];
+    this->groupsSize = size;
+    groups = new Group[groupsSize];
 }
 
 Groups::~Groups()
 {
-	delete[] groups;
+    delete[] groups;
 }
 
 void Groups::showGroups()
 {
-	for (int i = 0; i < groupsSize; i++)
-		groups[i].show();
+    for (int i = 0; i < groupsSize; i++)
+        groups[i].show();
 }
 
 Group & Groups::operator[](int number) const
 {
-	return groups[number];
+    return groups[number];
 }
 
 void Groups::sortGroups()
 {
-	for (int i = 0; i < groupsSize - 1; i++) // ôóíêöèÿ ñîðòèðîâêè ïî àëôàâèòó(ïóçûðüêîâûé ìåòîä)
-	{
-		for (int j = 0; j < groupsSize - i - 1; j++)
-		{
-			if (groups[j].get().compare(groups[j + 1].get()) > 0) // compare - ñðàâíèâàåò ëåêñèêîãðàôè÷åñêè ñòðîêè
-				std::swap(groups[j], groups[j + 1]);
-		}
-	}
+    for (int i = 0; i < groupsSize - 1; i++) // ôóíêöèÿ ñîðòèðîâêè ïî àëôàâèòó(ïóçûðüêîâûé ìåòîä)
+    {
+        for (int j = 0; j < groupsSize - i - 1; j++)
+        {
+            if (groups[j].get().compare(groups[j + 1].get()) > 0) // compare - ñðàâíèâàåò ëåêñèêîãðàôè÷åñêè ñòðîêè
+                std::swap(groups[j], groups[j + 1]);
+        }
+    }
 
 }
 int Groups::getGroupsSize() const
 {
-	return groupsSize;
+    return groupsSize;
 }
 void Groups::setGroups()
 {
-	for (int i = 0; i < groupsSize; i++)
-		std::cin >> groups[i];
+    for (int i = 0; i < groupsSize; i++)
+        std::cin >> groups[i];
 }
 void Groups::setGroups(std::ifstream & fin)
 {
-	for (int i = 0; i < groupsSize; i++)
-		fin >> groups[i];
+    for (int i = 0; i < groupsSize; i++)
+        fin >> groups[i];
 }
 void Groups::outGroups()
 {
-	for (int i = 0; i < groupsSize; i++)
-		std::cout << groups[i];
+    for (int i = 0; i < groupsSize; i++)
+        std::cout << groups[i];
 }
-void Groups::outGroups(std::ofstream & fout)
+void Groups::outGroups(std::ofstream & fout) const
 {
-	for (int i = 0; i < groupsSize; i++)
-		fout << groups[i];
+    for (int i = 0; i < groupsSize; i++)
+        fout << groups[i];
 }
 #pragma endregion
 #pragma region Audiences
@@ -240,8 +240,8 @@ Audiences::Audiences()
 
 Audiences::Audiences(const int size)
 {
-	this->audiencesSize = size;
-	audiences = new Audience[audiencesSize];
+    this->audiencesSize = size;
+    audiences = new Audience[audiencesSize];
 }
 
 Audiences::~Audiences()
@@ -250,38 +250,38 @@ Audiences::~Audiences()
 
 void Audiences::showAudiences()
 {
-	for (int i = 0; i < audiencesSize; i++)
-		audiences[i].show();
+    for (int i = 0; i < audiencesSize; i++)
+        audiences[i].show();
 }
 
 Audience & Audiences::operator[](const int number) const
 {
-	return audiences[number];
+    return audiences[number];
 }
 
 int Audiences::getAudiencesSize() const
 {
-	return audiencesSize;
+    return audiencesSize;
 }
 void Audiences::setAudiences()
 {
-	for (int i = 0; i < audiencesSize; i++)
-		std::cin >> audiences[i];
+    for (int i = 0; i < audiencesSize; i++)
+        std::cin >> audiences[i];
 }
 void Audiences::setAudiences(std::ifstream & fin)
 {
-	for (int i = 0; i < audiencesSize; i++)
-		fin >> audiences[i];
+    for (int i = 0; i < audiencesSize; i++)
+        fin >> audiences[i];
 }
 void Audiences::outAudiences()
 {
-	for (int i = 0; i < audiencesSize; i++)
-		std::cout << audiences[i];
+    for (int i = 0; i < audiencesSize; i++)
+        std::cout << audiences[i];
 }
-void Audiences::outAudiences(std::ofstream & fout)
+void Audiences::outAudiences(std::ofstream & fout) const
 {
-	for (int i = 0; i < audiencesSize; i++)
-		fout << audiences[i];
+    for (int i = 0; i < audiencesSize; i++)
+        fout << audiences[i];
 }
 #pragma endregion
 #pragma region Subjects
@@ -292,61 +292,61 @@ Subjects::Subjects()
 
 Subjects::Subjects(const int size)
 {
-	this->subjectsSize = size;
-	subjects = new Subject[subjectsSize];
+    this->subjectsSize = size;
+    subjects = new Subject[subjectsSize];
 }
 
 Subjects::~Subjects()
 {
-	delete[] subjects;
+    delete[] subjects;
 }
 
 void Subjects::showSubjects()
 {
-	for (int i = 0; i < subjectsSize; i++)
-		subjects[i].show();
+    for (int i = 0; i < subjectsSize; i++)
+        subjects[i].show();
 }
 
 Subject & Subjects::operator[](const int number) const
 {
-	return subjects[number];
+    return subjects[number];
 }
 
 void Subjects::sortSubjects()
 {
-	for (int i = 0; i < subjectsSize - 1; i++) // ôóíêöèÿ ñîðòèðîâêè ïî àëôàâèòó(ïóçûðüêîâûé ìåòîä)
-	{
-		for (int j = 0; j < subjectsSize - i - 1; j++)
-		{
-			if (subjects[j].get().compare(subjects[j + 1].get()) > 0) // compare - ñðàâíèâàåò ëåêñèêîãðàôè÷åñêè ñòðîêè
-				std::swap(subjects[j], subjects[j + 1]);
-		}
-	}
+    for (int i = 0; i < subjectsSize - 1; i++) // ôóíêöèÿ ñîðòèðîâêè ïî àëôàâèòó(ïóçûðüêîâûé ìåòîä)
+    {
+        for (int j = 0; j < subjectsSize - i - 1; j++)
+        {
+            if (subjects[j].get().compare(subjects[j + 1].get()) > 0) // compare - ñðàâíèâàåò ëåêñèêîãðàôè÷åñêè ñòðîêè
+                std::swap(subjects[j], subjects[j + 1]);
+        }
+    }
 }
 
 int Subjects::getSubjectsSize() const
 {
-	return subjectsSize;
+    return subjectsSize;
 }
 void Subjects::setSubjects()
 {
-	for (int i = 0; i < subjectsSize; i++)
-		std::cin >> subjects[i];
+    for (int i = 0; i < subjectsSize; i++)
+        std::cin >> subjects[i];
 }
 void Subjects::setSubjects(std::ifstream & fin)
 {
-	for (int i = 0; i < subjectsSize; i++)
-		fin >> subjects[i];
+    for (int i = 0; i < subjectsSize; i++)
+        fin >> subjects[i];
 }
 void Subjects::outSubjects()
 {
-	for (int i = 0; i < subjectsSize; i++)
-		std::cout << subjects[i];
+    for (int i = 0; i < subjectsSize; i++)
+        std::cout << subjects[i];
 }
-void Subjects::outSubjects(std::ofstream & fout)
+void Subjects::outSubjects(std::ofstream & fout) const
 {
-	for (int i = 0; i < subjectsSize; i++)
-		fout << subjects[i];
+    for (int i = 0; i < subjectsSize; i++)
+        fout << subjects[i];
 }
 #pragma endregion
 #pragma region Specialities
@@ -357,64 +357,64 @@ Specialities::Specialities()
 
 Specialities::Specialities(const int size)
 {
-	this->specialitiesSize = size;
-	specialities = new Speciality[specialitiesSize];
+    this->specialitiesSize = size;
+    specialities = new Speciality[specialitiesSize];
 }
 
 Specialities::~Specialities()
 {
-	delete[] specialities;
+    delete[] specialities;
 }
 
 void Specialities::showSpecialities()
 {
-	for (int i = 0; i < specialitiesSize; i++)
-		specialities[i].show();
+    for (int i = 0; i < specialitiesSize; i++)
+        specialities[i].show();
 }
 
 Speciality & Specialities::operator[](const int number) const
 {
-	return specialities[number];
+    return specialities[number];
 }
 
 void Specialities::setSpecialities()
 {
-	for (int i = 0; i < specialitiesSize; i++)
-		std::cin >> specialities[i];
+    for (int i = 0; i < specialitiesSize; i++)
+        std::cin >> specialities[i];
 }
 
 void Specialities::setSpecialities(std::ifstream & fin)
 {
-	for (int i = 0; i < specialitiesSize; i++)
-		fin >> specialities[i];
+    for (int i = 0; i < specialitiesSize; i++)
+        fin >> specialities[i];
 }
 
 void Specialities::outSpecialities()
 {
-	for (int i = 0; i < specialitiesSize; i++)
-		std::cout << specialities[i];
+    for (int i = 0; i < specialitiesSize; i++)
+        std::cout << specialities[i];
 }
 
-void Specialities::outSpecialities(std::ofstream & fout)
+void Specialities::outSpecialities(std::ofstream & fout) const
 {
-	for (int i = 0; i < specialitiesSize; i++)
-		fout << specialities[i];
+    for (int i = 0; i < specialitiesSize; i++)
+        fout << specialities[i];
 }
 
 void Specialities::sortSpecialities()
 {
-	for (int i = 0; i < specialitiesSize - 1; i++) // ôóíêöèÿ ñîðòèðîâêè ïî àëôàâèòó(ïóçûðüêîâûé ìåòîä)
-	{
-		for (int j = 0; j < specialitiesSize - i - 1; j++)
-		{
-			if (specialities[j].get().compare(specialities[j + 1].get()) > 0) // compare - ñðàâíèâàåò ëåêñèêîãðàôè÷åñêè ñòðîêè
-				std::swap(specialities[j], specialities[j + 1]);
-		}
-	}
+    for (int i = 0; i < specialitiesSize - 1; i++) // ôóíêöèÿ ñîðòèðîâêè ïî àëôàâèòó(ïóçûðüêîâûé ìåòîä)
+    {
+        for (int j = 0; j < specialitiesSize - i - 1; j++)
+        {
+            if (specialities[j].get().compare(specialities[j + 1].get()) > 0) // compare - ñðàâíèâàåò ëåêñèêîãðàôè÷åñêè ñòðîêè
+                std::swap(specialities[j], specialities[j + 1]);
+        }
+    }
 }
 
 int Specialities::getSpecialitiesSize() const
 {
-	return specialitiesSize;
+    return specialitiesSize;
 }
 #pragma endregion
