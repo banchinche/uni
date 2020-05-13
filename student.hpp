1c348c6a96fe3abf8b4aa6a4a95531db4541b0a8
banchinche <45794357+banchinche@users.noreply.github.com>
2020-05-13 19:27:48 +0300

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -6,4 +6,33 @@ private:
     std::string studentSurname;
     Group studentGroup;
     Speciality studentSpeciality;
+    Subjects subjects;
+    Audiences audiences;
+public:
+    Student();
+    ~Student();
+    void show() const;
+    void showName() const;
+    void showSurname() const;
+    void showGroup() const;
+    void showSpeciality() const;
+    void showSubjects() const;
+    void showAudiences() const;
+
+    void set();
+    void set(std::string filename);
+    void setName();
+    void setName(std::string filename);
+    void setSurname();
+    void setSurname(std::string filename);
+    void setGroup();
+    void setGroup(std::string filename);
+    void setSpeciality();
+    void setSpeciality(std::string filename);
+    void setSubjects();
+    void setSubjects(std::string filename);
+    void setAudiences();
+    void setAudiences(std::string filename);
+
+
 };
